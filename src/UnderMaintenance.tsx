43339b7806274fae596e18e11d03cad17d2a5a19
unholyexcalibur911-cmyk <unholyexcalibur911@gmail.com>
2025-09-19 16:33:12 +0800

    Add multiple files

--- src/UnderMaintenance.tsx
+++ src/UnderMaintenance.tsx
@@ -4,18 +4,16 @@ import devImg from "./dev.jpg"; // Place your dev image as dev.jpg in src/
 
 const UnderMaintenance: React.FC = () => (
   <div className="dmc5-bg">
-    <div className="dmc5-loading-container">
+    <div className="dmc5-content">
       <div className="dmc5-left">
         <img src={devImg} alt="Developer" className="dmc5-dev-img" />
       </div>
       <div className="dmc5-right">
-        <h1 className="dmc5-title">UNDER MAINTENANCE</h1>
-        <div className="dmc5-tips">
-          <span className="dmc5-tips-label">TIPS</span>
-          <p>
-            The site is currently undergoing stylish upgrades.<br />
-            Please check back soon for more action!
-          </p>
+        <div className="dmc5-tips-title">TIPS</div>
+        <div className="dmc5-tips-bar"></div>
+        <div className="dmc5-tips-text">
+          The site is currently undergoing stylish upgrades.<br />
+          Please check back soon for more action!
         </div>
       </div>
     </div>
