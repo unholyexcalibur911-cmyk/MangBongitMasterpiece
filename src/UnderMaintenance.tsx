4b58edb1656ef3b0320fded502c8ae68a7be0ed6
unholyexcalibur911-cmyk <unholyexcalibur911@gmail.com>
2025-09-19 16:17:02 +0800

    Add multiple files

--- src/UnderMaintenance.tsx
+++ src/UnderMaintenance.tsx
@@ -2,16 +2,19 @@ import React from "react";
 import "./UnderMaintenance.css";
 
 const UnderMaintenance: React.FC = () => (
-  <div className="dmc-bg">
-    <div className="dmc-card">
-      <div className="dmc-icon">🩸</div>
-      <h1 className="dmc-title">UNDER MAINTENANCE</h1>
-      <p className="dmc-message">
-        <span className="dmc-red">The gates are sealed.</span><br />
-        Our crew is slaying bugs and demons.<br />
-        <span className="dmc-blue">Check back soon for stylish action!</span>
+  <div className="dmc5-bg">
+    <div className="dmc5-card">
+      <div className="dmc5-loader">
+        <span className="dmc5-loader-icon">🗡️</span>
+        <span className="dmc5-loader-spinner"></span>
+      </div>
+      <h1 className="dmc5-title">UNDER MAINTENANCE</h1>
+      <p className="dmc5-message">
+        <span className="dmc5-red">The inner demons hunt is on.</span><br />
+        Our team is unleashing SSS style fixes.<br />
+        <span className="dmc5-blue">Please check back soon for more stylish action!</span>
       </p>
-      <div className="dmc-footer">
+      <div className="dmc5-footer">
         <span>AyaSync Team</span>
       </div>
     </div>
