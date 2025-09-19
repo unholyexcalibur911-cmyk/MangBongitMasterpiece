1c17e2ea7634fb70e4c8a85231ccb1ceb5764213
unholyexcalibur911-cmyk <unholyexcalibur911@gmail.com>
2025-09-19 16:47:00 +0800

    UnderMaintenance.tsx

--- src/UnderMaintenance.tsx
+++ src/UnderMaintenance.tsx
@@ -20,10 +20,7 @@ const UnderMaintenance: React.FC = () => {
             setShowJCE(false);
             setProgress(0);
             intervalRef.current = window.setInterval(() => {
-              setProgress((p) => (p >= 100 ? p : p + Math.random() * 0.2 + 0.01)); 
-                if(p >= 50){
-                  setProgress((p) => (p >= 100 ? p : p + Math.random() * 0.05 + 0.001));
-                }// slower increment
+              setProgress((p) => (p >= 100 ? p : p + Math.random() * 0.02 + 0.001));
             }, 50); // slower interval
           }, JCE_ANIMATION_DURATION);
           return 100;
